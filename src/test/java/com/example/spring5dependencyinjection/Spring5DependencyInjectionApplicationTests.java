a6df82804b9f52e823ecd7c3bbe55f0498d6b184
Mateusz Siekierka <mat.siekierka96@gmail.com>
2020-10-07 14:37:18 +0200

    first commit

---
+++ src/test/java/com/example/spring5dependencyinjection/Spring5DependencyInjectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring5dependencyinjection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5DependencyInjectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

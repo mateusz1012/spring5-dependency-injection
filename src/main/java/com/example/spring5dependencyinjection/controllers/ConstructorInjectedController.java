2a5c407551e1f3a7b7e9d1273c9d8d00227c64fb
Mateusz Siekierka <mat.siekierka96@gmail.com>
2020-10-08 12:59:10 +0200

    qualifiers

--- src/main/java/com/example/spring5dependencyinjection/controllers/ConstructorInjectedController.java
+++ src/main/java/com/example/spring5dependencyinjection/controllers/ConstructorInjectedController.java
@@ -2,6 +2,7 @@ package com.example.spring5dependencyinjection.controllers;
 
 import com.example.spring5dependencyinjection.services.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -10,7 +11,7 @@ public class ConstructorInjectedController {
     private final GreetingService greetingService;
 
     //@Autowired nie wymagane już
-    public ConstructorInjectedController(GreetingService greetingService) {
+    public ConstructorInjectedController(@Qualifier("constructorGreetingService") GreetingService greetingService) {
         this.greetingService = greetingService;
     }
 
--- src/test/java/com/example/spring5dependencyinjection/controllers/SetterInjectedControllerTest.java
+++ src/test/java/com/example/spring5dependencyinjection/controllers/SetterInjectedControllerTest.java
@@ -1,10 +1,8 @@
 package com.example.spring5dependencyinjection.controllers;
 
-import com.example.spring5dependencyinjection.services.GreetingServiceImpl;
+import com.example.spring5dependencyinjection.services.ConstructorGreetingService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class SetterInjectedControllerTest {
 
@@ -13,7 +11,7 @@ class SetterInjectedControllerTest {
     @BeforeEach
     void setUp() {
         setterInjectedController = new SetterInjectedController();
-        setterInjectedController.setGreetingService(new GreetingServiceImpl());
+        setterInjectedController.setGreetingService(new ConstructorGreetingService());
     }
 
     @Test

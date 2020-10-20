2e68f4a92406213474f5e8438acffde141c005f0
Mateusz Siekierka <mat.siekierka96@gmail.com>
2020-10-20 12:27:52 +0200

    adding property source example

---
+++ src/main/java/com/example/spring5dependencyinjection/config/PropertyConfig.java
@@ -0,0 +1,40 @@
+package com.example.spring5dependencyinjection.config;
+
+import com.example.spring5dependencyinjection.examplebeans.FakeDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+@Configuration
+@PropertySource("classpath:datasource.properties")
+public class PropertyConfig {
+
+    @Value("${mat.username}")
+    String user;
+
+    @Value("${mat.password}")
+    String password;
+
+    @Value("${mat.dburl}")
+    String url;
+
+    @Bean
+    public FakeDataSource fakeDataSource() {
+        FakeDataSource fakeDataSource = new FakeDataSource();
+
+        fakeDataSource.setUsername(user);
+        fakeDataSource.setPassword(password);
+        fakeDataSource.setUrl(url);
+
+        return fakeDataSource;
+    }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer properties() {
+        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
+
+        return propertySourcesPlaceholderConfigurer;
+    }
+}

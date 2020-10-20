d5957506bc324edde91b8143ea980ba94305f591
Mateusz Siekierka <mat.siekierka96@gmail.com>
2020-10-20 16:46:15 +0200

    adding multiple property files

--- src/main/java/com/example/spring5dependencyinjection/Spring5DependencyInjectionApplication.java
+++ src/main/java/com/example/spring5dependencyinjection/Spring5DependencyInjectionApplication.java
@@ -2,6 +2,7 @@ package com.example.spring5dependencyinjection;
 
 import com.example.spring5dependencyinjection.controllers.MyController;
 import com.example.spring5dependencyinjection.examplebeans.FakeDataSource;
+import com.example.spring5dependencyinjection.examplebeans.FakeJmsBroker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -15,8 +16,10 @@ public class Spring5DependencyInjectionApplication {
 		MyController myController = (MyController) ctx.getBean("myController");
 
 		FakeDataSource fakeDataSource = (FakeDataSource) ctx.getBean(FakeDataSource.class);
-
 		System.out.println(fakeDataSource.getUsername());
+
+		FakeJmsBroker fakeJmsBroker = (FakeJmsBroker) ctx.getBean(FakeJmsBroker.class);
+		System.out.println(fakeJmsBroker.getUsername());
 	}
 
 }
--- src/main/java/com/example/spring5dependencyinjection/config/PropertyConfig.java
+++ src/main/java/com/example/spring5dependencyinjection/config/PropertyConfig.java
@@ -1,14 +1,21 @@
 package com.example.spring5dependencyinjection.config;
 
 import com.example.spring5dependencyinjection.examplebeans.FakeDataSource;
+import com.example.spring5dependencyinjection.examplebeans.FakeJmsBroker;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @Configuration
-@PropertySource("classpath:datasource.properties")
+//są dwie możliwości użycia PropertySource
+//@PropertySource({"classpath:datasource.properties", "classpath:jms.properties"})
+@PropertySources({
+        @PropertySource("classpath:datasource.properties"),
+        @PropertySource("classpath:jms.properties")
+})
 public class PropertyConfig {
 
     @Value("${mat.username}")
@@ -20,6 +27,15 @@ public class PropertyConfig {
     @Value("${mat.dburl}")
     String url;
 
+    @Value("${mat.jms.username}")
+    String jmsUsername;
+
+    @Value("${mat.jms.password}")
+    String jmsPassword;
+
+    @Value("${mat.jms.url}")
+    String jmsUrl;
+
     @Bean
     public FakeDataSource fakeDataSource() {
         FakeDataSource fakeDataSource = new FakeDataSource();
@@ -29,6 +45,17 @@ public class PropertyConfig {
         fakeDataSource.setUrl(url);
 
         return fakeDataSource;
+    }
+
+    @Bean
+    public FakeJmsBroker fakeJmsBroker() {
+        FakeJmsBroker fakeJmsBroker = new FakeJmsBroker();
+
+        fakeJmsBroker.setUsername(jmsUsername);
+        fakeJmsBroker.setPassword(jmsPassword);
+        fakeJmsBroker.setUrl(jmsUrl);
+
+        return fakeJmsBroker;
     }
 
     @Bean
